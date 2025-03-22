61a2421ee6a27e087de4363b60bd43b54f088a9a
Reghan Lao <rl907@kill.cs.rutgers.edu>
2025-03-22 11:38:23 -0400

    minor print change

--- outlier.c
+++ outlier.c
@@ -3,6 +3,6 @@
 
 int main(int argc, char *argv[]) {
   if(argc < 2) {
-    fprintf(stderr, "Usage: %s <files(s) or directory/directories>", argv[0]);
+    fprintf(stderr, "Usage: %s <files(s) and/or directory/directories>", argv[0]);
   }
 }
